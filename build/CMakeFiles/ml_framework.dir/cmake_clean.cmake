cb98a565653b1fb3912e83cbe8fa46f30836e3d2
Sarthak Garg <sarthak@Sarthaks-MacBook-Air.local>
2024-10-21 21:41:33 +0530

    csv parser implemented

---
+++ build/CMakeFiles/ml_framework.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ml_framework.dir/src/read_data.cpp.o"
+  "CMakeFiles/ml_framework.dir/src/read_data.cpp.o.d"
+  "ml_framework"
+  "ml_framework.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ml_framework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/read_data.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class DataFrame{
+    private : 
+        vector<vector<double> > dataFrame;
+
+    public :
+        void parser(string path){
+            string line;
+
+            ifstream dataFile(path);
+            try{
+                if (!dataFile.is_open()){
+                    throw runtime_error("File not found");
+                }
+
+                int lineCnt = 1;
+
+                while (getline(dataFile,line)){
+                    vector<double> row;
+                    string currEntry;
+                    for (int i = 0; i<line.size(); i++){
+                        char c = line[i];
+                        if (c == ','){
+                            try{
+                                row.push_back(stod(currEntry));
+                            }
+                            catch(exception& e){
+                                string error = "Invalid data in line : " + to_string(lineCnt);
+                                throw runtime_error(error);
+                            }
+                            currEntry.clear();
+                        }else{
+                            currEntry += c;
+                        }
+                    }
+                    try{
+                        row.push_back(stod(currEntry));
+                    }catch(exception& e){
+                        string error = "Invalid data in line : " + to_string(lineCnt);
+                        throw runtime_error(error);
+                    }
+                    dataFrame.push_back(row);
+                    currEntry.clear();
+                    lineCnt++;
+                }
+            } catch(exception& e){
+                cerr<< "Error while parsing data file : "<< e.what() << endl;
+            }
+            dataFile.close();
+
+        }
+    
+        void displayData(){
+            for (auto &row : dataFrame){
+                for (auto &value : row){
+                    cout<< value << " ";
+                }
+            }
+        }
+};
+
+int main(){
+    
+    DataFrame df;
+    df.parser("/Users/sarthak/projects/ml_framework/trial.csv");
+    df.displayData();
+    return 0;
+
+
+}
