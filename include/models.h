4c91fa16b13cd8a30edab8909b24bd230df08f8a
Sarthak Garg <sarthak@Sarthaks-MacBook-Air.local>
2024-11-04 23:04:42 +0530

    added models.h

---
+++ include/models.h
@@ -0,0 +1,12 @@
+#ifndef MOD
+#define MOD
+
+#include "linearRegression.h"
+#include "logisticRegression.h"
+#include "dimensionalityReduction.h"
+#include "KNNclass.h"
+#include "KNNreg.h"
+#include "KMeans.h"
+
+
+#endif
