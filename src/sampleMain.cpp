0dfd454cd6084519d7fa3a864c30463c9beb6d49
Sarthak Garg <sarthak@Sarthaks-MacBook-Air.local>
2024-11-05 13:09:30 +0530

    addded metrics

--- src/sampleMain.cpp
+++ src/sampleMain.cpp
@@ -4,16 +4,16 @@
 
 using namespace std;
 
-int main(){
-    DataFrame df;
-    try{
-        df.parseData("path_to_csv_file.csv",true);
-    }catch(exception& e){
-        cout<<"Error : "<<e.what()<<endl;
-        return 0;
-    }
-    df.displayData();
+// int main(){
+//     DataFrame df;
+//     try{
+//         df.parseData("path_to_csv_file.csv",true);
+//     }catch(exception& e){
+//         cout<<"Error : "<<e.what()<<endl;
+//         return 0;
+//     }
+//     df.displayData();
 
-    PCA pca;
-    cout << pca.fit_transform("path_to_csv_file.csv",5) << endl;
-}
+//     PCA pca;
+//     cout << pca.fit_transform("path_to_csv_file.csv",5) << endl;
+// }
